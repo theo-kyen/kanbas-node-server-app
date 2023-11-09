7acf226e3a3c378f7a62b5d8e611fd6521703924
Theodore Kyenkyenhene <tkyenkyenhene21@gmail.com>
2023-11-09 15:10:17 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import "dotenv/config"
+import express from "express";
+import cors from "cors";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+}));
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
