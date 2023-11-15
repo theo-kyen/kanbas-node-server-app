887529c79e5fa3af261fd68dc47f26a28dd43d9e
Theodore Kyenkyenhene <tkyenkyenhene21@gmail.com>
2023-11-14 23:11:40 -0500

    Finished A5

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 
 const app = express();
-const options = {
-  credentials: true,
-  origin: process.env.FRONTEND_URL,
-};
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
